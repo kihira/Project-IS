b06b8af1b1b647130989363bbc11aec90757ac45
Kihira <zoe.kihira.lee@gmail.com>
2014-04-20 10:40:22 +0100

    Base exo suit classes

---
+++ src/main/java/kihira/projectis/item/armour/ItemExoHelmet.java
@@ -0,0 +1,10 @@
+package kihira.projectis.item.armour;
+
+import net.minecraft.item.ItemArmor;
+
+public class ItemExoHelmet extends ItemArmor {
+
+    public ItemExoHelmet(ArmorMaterial p_i45325_1_, int p_i45325_2_, int p_i45325_3_) {
+        super(p_i45325_1_, p_i45325_2_, p_i45325_3_);
+    }
+}
---
+++ src/main/java/kihira/projectis/item/armour/ItemExoLegs.java
@@ -0,0 +1,10 @@
+package kihira.projectis.item.armour;
+
+import net.minecraft.item.ItemArmor;
+
+public class ItemExoLegs extends ItemArmor {
+
+    public ItemExoLegs(ArmorMaterial p_i45325_1_, int p_i45325_2_, int p_i45325_3_) {
+        super(p_i45325_1_, p_i45325_2_, p_i45325_3_);
+    }
+}
